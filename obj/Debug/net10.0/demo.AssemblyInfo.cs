4612a5df4ee7477c3dbbcbd1c8de061135a9236e
David Kouadio <davidboti.f.k@gmail.com>
2026-07-06 10:19:23 +0100

    teste

--- obj/Debug/net10.0/demo.AssemblyInfo.cs
+++ obj/Debug/net10.0/demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc23d0390b765573e20e99b567963cd1ad499b43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c11edcc1416287bebc1c18437b9defc3146dc152")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
